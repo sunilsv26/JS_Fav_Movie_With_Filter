e9e89a2d48dfc350f5593acbb7b96260eef2cceb
Sunil <sunilsv26@gmail.com>
2020-07-23 12:42:33 +0530

    rendered movielist on html page

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -2,6 +2,40 @@ const addMovieBtn = document.getElementById('add-movie-btn');
 const searchFilterBtn = document.getElementById('search-btn');
 const movies = [];
 
+const movieRender = ()=>{
+    const movieList = document.getElementById('movie-list');
+
+    if(movies.length === 0){
+        movieList.classList.remove('visible');
+    }
+    else{
+        movieList.classList.add('visible');
+    }
+    movieList.innerHTML ='';
+   /*  movies.forEach((movie)=>{
+        const movieEl = document.createElement('li');
+        let text = movie.info.title + '-';
+            for(const key in movie.info){
+                if(key !== 'title'){
+                    text = text + `${key}:${movie.info[key]}`
+                }
+            }
+    movieEl.textContent = text
+    movieList.append(movieEl);
+    }) */
+    
+    for(const movie of movies){
+        const movieEl = document.createElement('li');
+        let text = movie.info.title + '-';
+            for(const key in movie.info){
+                if(key !== 'title'){
+                    text = text + `${key}:${movie.info[key]}`
+                }
+            }
+    movieEl.textContent = text
+    movieList.append(movieEl);
+    }
+}
 const addMovieHandler = ()=>{
     const title = document.getElementById('title').value;
     const extraName = document.getElementById('extra-name').value;
@@ -15,6 +49,7 @@ const addMovieHandler = ()=>{
     }
     movies.push(newMovie);
     console.log(movies);
+    movieRender();
 
 }
 
